a1ba7434beaf0fb33e160216452ed82d40110e1a
Darío Marmie <mrdarkiox@gmail.com>
2024-10-02 22:35:30 -0300

    Upload kickstart config

---
+++ lua/core/options.lua
@@ -0,0 +1,26 @@
+vim.wo.number = true
+vim.o.relativenumber = true
+vim.o.clipboard = 'unnamedplus'
+vim.o.wrap = false
+vim.o.linebreak = true
+vim.o.mouse = 'a'
+vim.o.autoindent = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.showtabline = 2
+vim.o.smartindent = true
+vim.o.cursorline = false
+vim.o.numberwidth = 4
+
+
+
+
+
+
+
+
+
